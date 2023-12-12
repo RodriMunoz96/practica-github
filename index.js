ad16d13eb57810a1a00831e23fc9c520e119e1c5
RodriMunoz96 <rodrigo.gaston.munoz@gmail.com>
2023-12-12 18:06:05 -0300

    funcion

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+  if (isNaN(numero)) {
+    return "Debe ser un número entero";
+  } else {
+    if (numero % 2 === 0) {
+      return numero.toString(2);
+    } else {
+      return numero.toString(16);
+    }
+  }
 }
