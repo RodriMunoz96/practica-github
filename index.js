7702b4196ecb7e4df67c04b301e2552b0a8217a6
Julian Guzman <jhguzmans@unal.edu.co>
2023-12-12 16:08:03 -0500

    laFuncion

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+  if (Number.isInteger(numero)) {
+    if (numero % 2 === 0) {
+      // Si es un número par, convertir a binario
+      return numero.toString(2);
+    } else {
+      // Si es un número impar, convertir a hexadecimal
+      return numero.toString(16);
+    }
+  } else {
+    // Manejar el caso en el que el argumento no sea un número entero
+    return "Por favor, proporciona un número entero.";
+  }
 }
